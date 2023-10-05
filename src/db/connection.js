4e2628008157faf78a278bc33f7ddc8d4735fb03
Rafael Pegoretti <rpegoretti@vmware.com>
2023-10-05 20:57:26 -0300

    adding docker compose

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'mysecretpassword', {
+const sequelize = new Sequelize('postgres', 'postgres', 'password', {
     dialect: 'postgres',
     host: 'localhost',
     port: 5432
